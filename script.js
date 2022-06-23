8e376470bd0eef8e00686b43351e76f6f9834608
chip-e <chip.e.98@protonmail.com>
2022-06-23 17:54:25 -0500

    Update buttons, text display

--- script.js
+++ script.js
@@ -1,41 +1,44 @@
-function playerSelection() {
-  let selection = prompt("rock, paper, or scissors?");
-  return selection.toLowerCase();
-}
-
+const choices = document.querySelectorAll('button');
+const cpuDisplay = document.getElementById('cpu-display');
+const winDisplay = document.getElementById('win-display');
+const playerDisplay = document.getElementById('player-display');
+const playerScoreDisplay = document.getElementById('player-score');
+const cpuScoreDisplay = document.getElementById('cpu-score');
+const tieDisplay = document.getElementById('ties');
+let player;
+let playerScore = 0;
+let cpuScore = 0; 
+let tieScore = 0;
+let maxScore = 5;
+ 
 function cpuSelection() {
   let randNum = Math.floor(Math.random() * 3) + 1
+  if (randNum === 1) cpuChoice = 'ROCK'
+  if (randNum === 2) cpuChoice = 'PAPR'
+  if (randNum === 3) cpuChoice = 'SCSR' 
+  return cpuChoice;
+} 
+choices.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    player = e.target.id;
+    playerDisplay.innerHTML = player;
+    cpu = cpuSelection();
+    cpuDisplay.innerHTML = cpu;
+    let gameResult = (player, cpu) => {
+      if (player == 'ROCK' && cpu == 'SCSR' || player == 'PAPR' && cpu == 'ROCK' || player == 'SCSR' && cpu == 'PAPR') {
+        playerScore++;    //tie
+      } else if (player == cpu) {
+        tieScore++ //change to msg on page
+      } else {    //lose
+        cpuScore++;
+      }
+    };
+    gameResult(player, cpu);
+    playerScoreDisplay.innerHTML = playerScore;
+    cpuScoreDisplay.innerHTML = cpuScore;
+    tieDisplay.innerHTML = tieScore;
+    //if (playerScore === 5) 
+    //if (cpuScore === 5)
 
-  if (randNum === 1) {
-    selection = 'rock'
-  } if (randNum === 2) {
-    selection = 'paper'
-  } if (randNum === 3) {
-    selection = 'scissors'
-  }
-
-  return selection;
-}
-
-function gameResult(player, cpu) {
- if (player == 'rock' && cpu == 'scissors') {
-  console.log('player wins')
- } else if (player == 'paper' && cpu == 'rock') {
-  console.log('player wins')
- } else if (player == 'scissors' && cpu == 'paper') {
-  console.log('player wins')
- } else if (player == cpu) {
-  console.log('tie')
- } else {
-  console.log('player loses')
- } 
-}
-
-for (let i = 0; i < 5; i++) {
-let player = playerSelection();
-let cpu = cpuSelection();
-
-console.log(`Player's choice: ${player}`);
-console.log(cpu);
-gameResult(player, cpu);
-}
+  });
+});
